65b914da89e677ccde282949243114e0bf6e232e
alicesin615 <alicesin0615@gmail.com>
2023-05-14 23:49:00 +0800

    create button component & brand icon

---
+++ src/components/Buttons/Button.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+    icon?: React.ReactNode;
+    label: string;
+    variant?: 'primary' | 'secondary';
+};
+
+export function Button({
+    label,
+    color,
+    icon,
+    variant,
+    className,
+    ...rest
+}: ButtonProps) {
+    return (
+        <button
+            {...rest}
+            color={color}
+            className={`px-3 py-2 rounded-md leading-6 text-white bg-brand-primary shadow-slate-700 shadow border-none font-medium w-auto select-none touch-manipulation transition-all hover:bg-brand-primary-light focus:!outline-none ${
+                variant === 'primary' ? '' : ''
+            } ${className ? className : ''}`}
+        >
+            {icon && <i className="icon">{icon}</i>}
+            {label}
+        </button>
+    );
+}
---
+++ src/components/Icons/index.ts
@@ -0,0 +1,1 @@
+export {MinterestIcon} from './Icons'
