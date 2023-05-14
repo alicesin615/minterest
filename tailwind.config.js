e3fd3cb59dafbdf523c0fabf679981d90a983730
alicesin615 <alicesin0615@gmail.com>
2023-05-14 23:50:00 +0800

    add ethers to global type & adj configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
+import plugin from 'tailwindcss';
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {
             colors: {
-                system: '#141931'
+                system: '#141931',
+                'brand-primary': '#0891b2',
+                'brand-primary-light': '#3fbbd9',
+                'brand-primary-muted': 'rgba(8, 145, 178,0.6)',
+                'brand-secondary': '#db2777'
             }
         }
     },
-    plugins: []
+    plugins: [
+        plugin(function ({ addVariant, addUtilities }) {
+            addVariant('active-class', '&.active');
+        })
+    ]
 };
