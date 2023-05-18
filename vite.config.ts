49ff2af69c4470425dd69c592c8a6203d54ae551
alicesin615 <alicesin0615@gmail.com>
2023-05-19 00:06:05 +0800

    let vite recognise env variables + add env example

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 
+dotenv.config() // load env vars from .env
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': process.env,
+    INFURA_PUBLIC_KEY: `"${process.env.INFURA_PUBLIC_KEY}"`
+  },
   resolve: {
     alias: {
       
