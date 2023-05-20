3cafcf97a9a9f47cc465c06c9a4184b2f105e8f9
alicesin615 <alicesin0615@gmail.com>
2023-05-21 01:08:46 +0800

    add some alias paths + eth icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+import * as path from 'path';
 
-dotenv.config() // load env vars from .env
+dotenv.config(); // load env vars from .env
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': process.env,
-    INFURA_PUBLIC_KEY: `"${process.env.INFURA_PUBLIC_KEY}"`
-  },
-  resolve: {
-    alias: {
-      
+    plugins: [react()],
+    define: {
+        'process.env': process.env,
+        INFURA_PUBLIC_KEY: `"${process.env.INFURA_PUBLIC_KEY}"`
+    },
+    resolve: {
+        alias: {
+            '@components': `${path.resolve(__dirname, './src/components/')}`,
+            '@pages': path.resolve(__dirname, './src/pages'),
+            '@utils': path.resolve(__dirname, './src/utils')
+        }
     }
-  }
-})
+});
