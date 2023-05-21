7cc2aa4bf4f562ac646cedf0c008c7b1353204d2
alicesin615 <alicesin0615@gmail.com>
2023-05-22 00:01:18 +0800

    add reusable components and styles

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -19,8 +19,13 @@ export function Button({
             {...rest}
             color={color}
             className={`px-3 py-1.5 text-sm rounded-md leading-6 text-white bg-brand-primary-muted shadow-slate-700 shadow border-none font-medium w-auto select-none touch-manipulation transition-all hover:bg-brand-primary-light focus:!outline-none ${
-                variant === 'primary' ? '' : ''
-            } ${className ? className : ''}`}
+                variant === 'primary' && ''
+            }
+            ${
+                variant === 'secondary' &&
+                'bg-brand-secondary hover:bg-brand-secondary/60'
+            }
+            ${className ? className : ''}`}
         >
             {icon && <i className="icon">{icon}</i>}
             {label}
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -4,5 +4,19 @@ export const PrimaryText = React.forwardRef<
     HTMLSpanElement,
     React.HTMLAttributes<HTMLSpanElement>
 >(({ className, ...rest }: React.HTMLAttributes<HTMLSpanElement>, ref) => (
-    <span className={`text-slate-4 ${className || ''}`} ref={ref} {...rest} />
+    <span className={`text-slate-2 ${className || ''}`} ref={ref} {...rest} />
+));
+
+export const SecondaryText = React.forwardRef<
+    HTMLSpanElement,
+    React.HTMLAttributes<HTMLSpanElement>
+>(({ className, ...rest }: React.HTMLAttributes<HTMLSpanElement>, ref) => (
+    <span className={`text-slate-5 ${className || ''}`} ref={ref} {...rest} />
+));
+
+export const MutedText = React.forwardRef<
+    HTMLSpanElement,
+    React.HTMLAttributes<HTMLSpanElement>
+>(({ className, ...rest }: React.HTMLAttributes<HTMLSpanElement>, ref) => (
+    <span className={`text-slate-6 ${className || ''}`} ref={ref} {...rest} />
 ));
