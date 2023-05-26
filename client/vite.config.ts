741d6d9a218ec9f14c95f8ddded1576e3c504d58
alicesin615 <alicesin0615@gmail.com>
2023-05-27 02:09:16 +0800

    update alias path

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -15,7 +15,8 @@ export default defineConfig({
         alias: {
             '@components': `${path.resolve(__dirname, './src/components/')}`,
             '@pages': path.resolve(__dirname, './src/pages'),
-            '@utils': path.resolve(__dirname, './src/utils')
+            '@utils': path.resolve(__dirname, './src/utils'),
+            '@constants': path.resolve(__dirname, './src/constants')
         }
     }
 });
