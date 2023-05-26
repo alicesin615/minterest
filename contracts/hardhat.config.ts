9c77a9e4568df2398cb880696d5fd4841e87b4d9
alicesin615 <alicesin0615@gmail.com>
2023-05-27 02:08:45 +0800

    update hardhat configs

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,9 +1,10 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
-require('dotenv').config({ path: '../../Minterest/.env' });
+require('dotenv').config({ path: '../../Minterest/contracts/.env' });
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || '';
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '';
 
 const config: HardhatUserConfig = {
     solidity: '0.8.19',
@@ -21,13 +22,12 @@ const config: HardhatUserConfig = {
         },
         sepolia: {
             url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-            // accounts: {},
+            accounts: [PRIVATE_KEY],
             chainId: 11155111
         }
     },
     etherscan: {
         apiKey: {
-            mainnet: ETHERSCAN_API_KEY,
             sepolia: ETHERSCAN_API_KEY
         }
     }
