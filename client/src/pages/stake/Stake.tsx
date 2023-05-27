9a23b6670b0373529ee382a9c45069d5da7bedff
alicesin615 <alicesin0615@gmail.com>
2023-05-28 01:56:12 +0800

    minor update to stake component

--- client/src/pages/stake/Stake.tsx
+++ client/src/pages/stake/Stake.tsx
@@ -2,19 +2,27 @@ import { useLocation } from 'react-router-dom';
 import { Card, PrimaryText } from '../../components';
 import { SigFigFormatter } from '../../utils';
 
+type StakeProps = {};
 export function Stake() {
     return (
-        <div className="flex gap-8">
-            <Card title="Currently Staked" className="flex-1">
-                <div className="flex gap-2 pt-2">
-                    <div className="text-brand-primary font-medium text-2xl">
-                        {SigFigFormatter(0)}
+        <div className="flex flex-col gap-8">
+            <div className="flex gap-8">
+                <Card title="Currently Staked" className="flex-1">
+                    <div className="flex gap-2 pt-2">
+                        <div className="text-brand-primary font-medium text-2xl">
+                            {SigFigFormatter(0)}
+                        </div>
                     </div>
-                </div>
-            </Card>
-            <Card title="Available to Stake" className="flex-1">
-                <div>{SigFigFormatter(0)}</div>
-            </Card>
+                </Card>
+                <Card title="Available to Stake" className="flex-1">
+                    <div className="flex gap-2 pt-2">
+                        <div className="text-brand-primary font-medium text-2xl">
+                            {SigFigFormatter(0)}
+                        </div>
+                    </div>
+                </Card>
+            </div>
+            <Card title="Available Collaterals" className="flex-1"></Card>
         </div>
     );
 }
