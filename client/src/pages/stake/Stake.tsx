62fc623645f38135bffec68cfa3009414a795207
alicesin615 <alicesin0615@gmail.com>
2023-06-01 01:00:31 +0800

    read tokens from stake contract & display on frontend

--- client/src/pages/stake/Stake.tsx
+++ client/src/pages/stake/Stake.tsx
@@ -1,11 +1,17 @@
 import { ethers } from 'ethers';
-import { useContractRead } from 'wagmi';
+import {
+    useContractRead,
+    useContractWrite,
+    usePrepareContractWrite
+} from 'wagmi';
+import { TextField } from '@mui/material';
 import { Card } from '@components/Card';
-import { PrimaryText, SecondaryText } from '@components/Text';
+import { MutedText, PrimaryText, SecondaryText } from '@components/Text';
 import * as priceFeed from '@constants/PriceFeed.json';
-import { PriceFeedAddress } from 'src/types';
+import * as stake from '@constants/Stake.json';
+import { PriceFeedAddress, Token } from 'src/types';
 import { Button } from '@components/Buttons';
-import { PriceFeedAddresses } from '@constants/PriceFeedAddresses';
+import { PriceFeedAddresses } from '@constants/addresses';
 import { SigFigFormatter } from '@utils/number-utils';
 import { Img } from '@components/Img';
 
@@ -15,7 +21,7 @@ const AvailableCollateral = ({
     priceFeedAddress: PriceFeedAddress;
 }) => {
     const { data: latestPriceData } = useContractRead({
-        address: priceFeed?.address,
+        address: priceFeed?.address as never,
         abi: priceFeed?.abi,
         functionName: 'getPrice',
         args: [Object.values(priceFeedAddress)?.[0]]
@@ -27,50 +33,80 @@ const AvailableCollateral = ({
         Boolean(latestPriceData) &&
         ethers.formatUnits(latestPriceData?.[0] as ethers.BigNumberish, 8);
 
-    // const retrievedTime = new Date(Number(latestPriceData?.[1]));
+    // const {
+    //     data,
+    //     isError,
+    //     isLoading,
+    //     write: stakeToken
+    // } = useContractWrite({
+    //     address: stake?.address as never,
+    //     abi: stake?.abi as never,
+    //     functionName: 'stakeTokens' as never,
+    //     args: ['LINK', 10] as never
+    // });
 
+    const { data: tokenData } = useContractRead({
+        address: stake?.address as never,
+        abi: stake?.abi,
+        functionName: 'getToken',
+        args: [symbol]
+    });
+
+    const { apy, name } = (tokenData as Token) || {};
+    const formattedApy = Number(apy) / 100;
     return (
-        <PrimaryText className="grid grid-cols-[repeat(3,1fr)] items-center py-4">
+        <PrimaryText className="grid grid-cols-[repeat(2,1fr),50px] items-center py-4">
             <div className="flex gap-4 items-center font-medium">
                 <Img
                     src={`/assets/images/coins/${symbol}.png`}
-                    width={32}
-                    height={32}
+                    width={36}
+                    height={36}
                 />
-                {symbol}
+                <div className="flex flex-col">
+                    {symbol}
+                    <MutedText className="text-xs">{name}</MutedText>
+                </div>
             </div>
             <span>{SigFigFormatter(formattedLatestPrice || 0)} USD</span>
-            <Button label="Stake" className="w-fit ml-auto" />
+            <span className="text-brand-primary">
+                {Boolean(tokenData) && SigFigFormatter(formattedApy || 0)} %
+            </span>
+            {/* <Button
+                label="Stake"
+                className="w-fit ml-auto"
+                onClick={stakeToken}
+            /> */}
         </PrimaryText>
     );
 };
 export function Stake() {
     return (
-        <div className="flex flex-col gap-8">
-            {/* <div className="flex gap-8">
-                <Card title="Currently Staked" className="flex-1">
-                    <div className="flex gap-2 pt-2">
-                        <div className="text-brand-primary font-medium text-2xl">
-                            {SigFigFormatter(0)}
-                        </div>
-                    </div>
-                </Card>
-                <Card title="Available to Stake" className="flex-1">
-                    <div className="flex gap-2 pt-2">
-                        <div className="text-brand-primary font-medium text-2xl">
-                            {SigFigFormatter(0)}
-                        </div>
-                    </div>
-                </Card>
-            </div> */}
-            <Card title="Available Collaterals" className="flex-1">
+        <div className="flex gap-8">
+            <Card title="Available Collaterals" className="w-3/5">
                 <div className="flex flex-col pt-4">
+                    <MutedText className="grid grid-cols-[repeat(2,1fr),50px] items-center py-1 [&_span]:mr-auto text-xs font-medium">
+                        <span>Symbol</span>
+                        <span>Price</span>
+                        <span>APY</span>
+                        <span></span>
+                    </MutedText>
                     {PriceFeedAddresses?.map((priceFeedAddress, i) => (
                         <AvailableCollateral
                             key={i}
                             priceFeedAddress={priceFeedAddress}
                         />
                     ))}
+                </div>
+            </Card>
+            <Card title="Stake" className="flex-1 ">
+                <div className="flex flex-col pt-4">
+                    <TextField
+                        id="number"
+                        type="number"
+                        required
+                        fullWidth
+                        placeholder="Choose Stake Amount"
+                    />
                 </div>
             </Card>
         </div>
