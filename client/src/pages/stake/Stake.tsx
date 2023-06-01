f5080fc30124c8e7869077de24fb97e4e9a2743f
alicesin615 <alicesin0615@gmail.com>
2023-06-02 02:10:52 +0800

    improve ux on staking

--- client/src/pages/stake/Stake.tsx
+++ client/src/pages/stake/Stake.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { ethers } from 'ethers';
 import {
     useContractRead,
@@ -9,16 +10,22 @@ import { Card } from '@components/Card';
 import { MutedText, PrimaryText, SecondaryText } from '@components/Text';
 import * as priceFeed from '@constants/PriceFeed.json';
 import * as stake from '@constants/Stake.json';
-import { PriceFeedAddress, Token } from 'src/types';
+import { PriceFeedAddress, Token, AvailableTokenSymbol } from 'src/types';
 import { Button } from '@components/Buttons';
+import { EmptyPlaceholder } from '@components/EmptyPlaceholder';
 import { PriceFeedAddresses } from '@constants/addresses';
 import { SigFigFormatter } from '@utils/number-utils';
+import { CheckIcon, EmptyCheckIcon } from '@components/Icons';
 import { Img } from '@components/Img';
 
 const AvailableCollateral = ({
-    priceFeedAddress
+    priceFeedAddress,
+    setTokenToStake,
+    tokenToStake
 }: {
     priceFeedAddress: PriceFeedAddress;
+    setTokenToStake?: (tokenToStake) => void;
+    tokenToStake?: string;
 }) => {
     const { data: latestPriceData } = useContractRead({
         address: priceFeed?.address as never,
@@ -55,7 +62,15 @@ const AvailableCollateral = ({
     const { apy, name } = (tokenData as Token) || {};
     const formattedApy = Number(apy) / 100;
     return (
-        <PrimaryText className="grid grid-cols-[repeat(2,1fr),50px] items-center py-4">
+        <PrimaryText
+            className={`grid grid-cols-[30px,repeat(2,1fr),50px] items-center py-4 hover:cursor-pointer transition-all`}
+            onClick={() => setTokenToStake(symbol)}
+        >
+            {symbol === tokenToStake ? (
+                <CheckIcon className="text-brand-primary" />
+            ) : (
+                <EmptyCheckIcon className="text-slate-600" />
+            )}
             <div className="flex gap-4 items-center font-medium">
                 <Img
                     src={`/assets/images/coins/${symbol}.png`}
@@ -71,43 +86,59 @@ const AvailableCollateral = ({
             <span className="text-brand-primary">
                 {Boolean(tokenData) && SigFigFormatter(formattedApy || 0)} %
             </span>
-            {/* <Button
-                label="Stake"
-                className="w-fit ml-auto"
-                onClick={stakeToken}
-            /> */}
         </PrimaryText>
     );
 };
 export function Stake() {
+    const [tokenToStake, setTokenToStake] =
+        useState<AvailableTokenSymbol | null>(null);
     return (
-        <div className="flex gap-8">
+        <div className="flex gap-6">
             <Card title="Available Collaterals" className="w-3/5">
                 <div className="flex flex-col pt-4">
-                    <MutedText className="grid grid-cols-[repeat(2,1fr),50px] items-center py-1 [&_span]:mr-auto text-xs font-medium">
+                    <MutedText className="grid grid-cols-[30px,repeat(2,1fr),50px] items-center py-1 [&_span]:mr-auto text-xs font-medium">
+                        <span></span>
                         <span>Symbol</span>
                         <span>Price</span>
                         <span>APY</span>
                         <span></span>
                     </MutedText>
-                    {PriceFeedAddresses?.map((priceFeedAddress, i) => (
-                        <AvailableCollateral
-                            key={i}
-                            priceFeedAddress={priceFeedAddress}
-                        />
-                    ))}
+                    {PriceFeedAddresses?.map(
+                        (priceFeedAddress: PriceFeedAddress, i: number) => (
+                            <AvailableCollateral
+                                key={i}
+                                priceFeedAddress={priceFeedAddress}
+                                setTokenToStake={setTokenToStake}
+                                tokenToStake={tokenToStake}
+                            />
+                        )
+                    )}
                 </div>
             </Card>
-            <Card title="Stake" className="flex-1 ">
-                <div className="flex flex-col pt-4">
-                    <TextField
-                        id="number"
-                        type="number"
-                        required
-                        fullWidth
-                        placeholder="Choose Stake Amount"
-                    />
+            <Card title="Stake" className="flex-1 flex-col flex ">
+                <div className="flex flex-col pt-4 h-full">
+                    {Boolean(tokenToStake) ? (
+                        <TextField
+                            id="number"
+                            type="number"
+                            required
+                            fullWidth
+                            placeholder="Choose Stake Amount"
+                            variant="standard"
+                            InputProps={{
+                                disableUnderline: true
+                            }}
+                        />
+                    ) : (
+                        <EmptyPlaceholder label="Please choose a token to stake" />
+                    )}
                 </div>
+                <Button
+                    label="Stake"
+                    className="w-full mt-auto"
+                    disabled={!tokenToStake}
+                    // onClick={stakeToken}
+                />
             </Card>
         </div>
     );
