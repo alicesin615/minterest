90b880497e36ae5e0031025843cbb42bffe95983
alicesin615 <alicesin0615@gmail.com>
2023-06-04 00:08:09 +0800

    minor css fixes & display assets fully

--- client/src/components/WalletInfo.tsx
+++ client/src/components/WalletInfo.tsx
@@ -1,25 +1,44 @@
+import React from 'react';
 import { useAccount, useBalance } from 'wagmi';
-import { Card, Img } from '.';
+import { useWalletBalance } from '@hooks/useWalletBalance';
+import { Card, Img, PrimaryText } from '.';
 import { SigFigFormatter } from '@utils/number-utils';
 
 export function WalletInfo() {
-    const { address } = useAccount();
-    const { data: balanceData, isLoading } = useBalance({
-        address
-    });
-    const { formatted, symbol } = balanceData || {};
+    const { address, isConnected } = useAccount();
+
+    const { sepoliaEthBalance, linkBalance } = useWalletBalance();
 
     return (
         <Card title="Wallet">
-            <div className="flex gap-4 pt-4 text-brand-primary font-medium text-2xl items-center">
-                {Boolean(symbol) && (
-                    <Img
-                        width={32}
-                        height={32}
-                        src={`/assets/images/coins/ETH.png`}
-                    />
+            <div className="flex gap-8 pt-4 text-brand-primary font-medium text-2xl items-center">
+                {!isConnected && (
+                    <PrimaryText className="text-sm !text-brand-secondary">
+                        Please connect your wallet first
+                    </PrimaryText>
                 )}
-                {SigFigFormatter(formatted || 0)}
+                {isConnected && (
+                    <React.Fragment>
+                        <div className="flex gap-2 items-center">
+                            <Img
+                                width={24}
+                                height={24}
+                                src={`/assets/images/coins/ETH.png`}
+                            />
+                            {SigFigFormatter(sepoliaEthBalance?.formatted || 0)}
+                        </div>
+                        <div className="flex gap-2 items-center">
+                            {Boolean(linkBalance?.symbol) && (
+                                <Img
+                                    width={24}
+                                    height={24}
+                                    src={`/assets/images/coins/${linkBalance?.symbol}.png`}
+                                />
+                            )}
+                            {SigFigFormatter(linkBalance?.formatted || 0)}
+                        </div>
+                    </React.Fragment>
+                )}
             </div>
         </Card>
     );
