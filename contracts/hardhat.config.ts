95897ef78c64ecee1183d44db5d48d030527bacc
alicesin615 <alicesin0615@gmail.com>
2023-06-05 00:08:23 +0800

    code cleaup

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,7 +1,7 @@
-import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
-require('dotenv').config({ path: '../../Minterest/contracts/.env' });
+import { HardhatUserConfig } from 'hardhat/config';
 require('@openzeppelin/hardhat-upgrades');
+require('dotenv').config({ path: '../../Minterest/contracts/.env' });
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || '';
